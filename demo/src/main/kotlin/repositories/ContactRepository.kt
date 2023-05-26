1b659e1b520d97ae843f4e90349b7381a5a59078
phgodoy <phgodoy12@gmail.com>
2023-05-26 08:57:22 -0300

    Delete demo directory

--- demo/src/main/kotlin/repositories/ContactRepository.kt
+++
@@ -1,9 +0,0 @@
-package repositories
-
-import entities.Contact;
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ContactRepository: JpaRepository<Contact, Long> {
-}
